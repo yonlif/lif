4ee0b4c74fd75fd03b38326f4467ae9aa15107f4
Yonatan Lifshitz <yonlif@gmail.com>
2021-04-05 02:18:50 +0300

    Setup files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='lif',
+    packages=['lif'],
+    version='0.1',
+    license='MIT',
+    description='Lif lib',
+    author='Yonlif & Eilonlif',
+    author_email='yonlif@gmail.com',
+    url='https://github.com/yonlif/lif',
+    download_url='https://github.com/yonlif/lif/archive/refs/tags/0.1.tar.gz',
+    keywords=['Lif'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
