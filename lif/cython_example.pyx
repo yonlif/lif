265b36915abd2f3d124efa21f689c49fc2d89c8c
Yonatan Lifshitz <yonlif@gmail.com>
2021-04-08 18:04:22 +0300

    fixed minor bug

--- lif/cython_example.pyx
+++ lif/cython_example.pyx
@@ -1,6 +1,6 @@
 # distutils: language=c++
 
-cdef extern from "cppmult.hpp":
+cdef extern from "cpp_code/cppmult.hpp":
     float cppmult_f(int int_param, float float_param)
 
 def pymult( int_param, float_param ):
